e5e3f78774a32df9d61b98e8484bb075d3fc141a
bksi37 <brakwasi619@gmail.com>
2025-05-12 10:00:35 -0500

    Add department and category filters to index.html and projects.js

--- migrate-users.js
+++ migrate-users.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
-const User = require('./models/User');
+const Project = require('./models/Project');
 
 mongoose.connect('mongodb+srv://brakwasi619:M6UcDlG5FiJylCEQ@cluster0.vbjwzl2.mongodb.net/sharecase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(async () => {
-    await User.updateMany(
-        { isProfileComplete: { $exists: false } },
-        { $set: { isProfileComplete: true } }
-    );
-    console.log('User migration complete');
+    await Project.updateMany({}, { $set: { likedBy: [] } });
+    console.log('Project migration complete');
     mongoose.disconnect();
 }).catch(err => console.error('Migration error:', err));
--- models/Project.js
+++ models/Project.js
@@ -4,21 +4,22 @@ const ProjectSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String },
     image: { type: String },
-    tags: [{ type: String }], // Already present
+    tags: [{ type: String }],
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     createdAt: { type: Date, default: Date.now },
-    problemStatement: { type: String }, // Added for project details
-    collaborators: [{ type: String }], // Added for collaborators list
-    resources: [{ type: String }], // Added for resource links
-    likes: { type: Number, default: 0 }, // Added for like count
-    views: { type: Number, default: 0 }, // Added for view count
-    comments: [{ // Added for comments
+    problemStatement: { type: String },
+    collaborators: [{ type: String }],
+    resources: [{ type: String }],
+    likes: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
+    comments: [{
         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
         userName: { type: String },
         text: { type: String, required: true },
         timestamp: { type: Date, default: Date.now }
     }],
-    isPublished: { type: Boolean, default: true } // Added to distinguish drafts
+    isPublished: { type: Boolean, default: true },
+    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] // Added for like/unlike
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
